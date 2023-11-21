3d5427695eb64b10c2917011fb6499983905889a
hianshul07 <hianshul07@gmail.com>
2023-11-21 23:21:48 +0530

    added a library

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
 fn main() {
     println!("Guess the number!!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
@@ -10,5 +15,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!!"),
+        Ordering::Equal => println!("You Win!!"),
+        Ordering::Greater => println!("Too big!!"),
+    }
 }
